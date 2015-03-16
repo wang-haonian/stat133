0adb8ff1d38e18b616f8e6ce2152249cf39a8c1c
wang-haonian <whn@berkeley.edu>
2015-03-15 22:13:46 -0700

    hw5 done

--- assignments/hw5/bml_functions.R
+++ assignments/hw5/bml_functions.R
@@ -8,10 +8,10 @@
 ## that stores the state of the system (i.e. location of red and blue cars)
 
 bml.init <- function(r, c, p){
-
-  
-   #return(m)
+  m <- matrix(sample(c(0,1,2),size=r*c,replace=T,prob=c((1-p),p/2,p/2)),nrow=r,ncol=c)
+  return(m)
 }
+
 
 #### Function to move the system one step (east and north)
 ## Input : a matrix [m] of the same type as the output from bml.init()
@@ -22,15 +22,61 @@ bml.init <- function(r, c, p){
 ## you can write extra functions that do just a step north or just a step east.
 
 bml.step <- function(m){
-
   
-   #return(list(m, grid.new))
+  move.right <- function(m) {
+    initial.matrix1 <- m
+    for (i in 1:nrow(m)){
+      for (j in 1:ncol(m)) {
+        if (j > 1)
+          if(j==ncol(m)) {
+            if(initial.matrix1[i,j]==1 && initial.matrix[i,1]==0)
+            {m[i,j]<-0 
+             m[i,1]<-1}
+          } else{
+            if(initial.matrix1[i,j]==1 && initial.matrix[i,j+1]==0)
+            {m[i,j]<-0 
+             m[i,j+1]<-1}}
+      }
+    }
+    return(m)
+  }
+  move.up <- function(m) {
+    initial.matrix2 <- m
+    for (i in 1:nrow(m)){
+      for (j in 1:ncol(m)) {
+        if(i==1) {if (initial.matrix2[1,j]==2 && initial.matrix2[nrow(m),j]==0)
+        {m[1,j]<-0
+         m[nrow(m),j]<-2} }
+        else{if (initial.matrix2[i,j]==2 && initial.matrix2[i-1,j]==0)
+        {m[i,j]<-0
+         m[i-1,j]<-2}}
+      }}
+    return(m)
+  }
+  initial.matrix <- m
+  m <- move.up(move.right(m))
+  grid.new <- !identical (initial.matrix,m)
+  return(list(m,grid.new))
+  #return(list(m, grid.new))
 }
 
 #### Function to do a simulation for a given set of input parameters
 ## Input : size of grid [r and c] and density [p]
 ## Output : *up to you* (e.g. number of steps taken, did you hit gridlock, ...)
 
-bml.sim <- function(r, c, p){
 
-}
+bml.sim <- function(r, c, p){
+  m <- bml.init(r,c,p)
+  for (i in 1:1200) { 
+    m <- bml.step(m)[[1]]
+    number.steps <- i
+    if (bml.step(m)[[2]]==F) break
+  }
+  return(number.steps)}
+bml.sim1 <- function(r, c, p){
+  for (i in 1:1200) { 
+    m <- bml.step(m)[[1]]
+    number.steps <- i
+    if (bml.step(m)[[2]]==F) break 
+  }
+  return(m)}
--- assignments/hw5/bml_simulation.R
+++ assignments/hw5/bml_simulation.R
@@ -8,3 +8,42 @@
 #### how many steps you observered before concluding that it is in a free flowing state.
 
 
+source("bml_functions.R")
+densities <- seq(0,1,by=0.1)
+#10*10
+op.density1 <- replicate(100, sapply(densities, function(p) bml.sim(10, 10, p)))
+op.average1 <- apply(op.density1, 1, mean)
+plot(densities, op.average1, xlab="density",ylab="how many steps to hit gridlock",main="10*10 matrix",
+     pch=".",cex.main=0.8, cex=6, col="blue")
+
+#10*15
+op.density2 <- replicate(100, sapply(densities, function(p) bml.sim(10, 15, p)))
+op.average2 <- apply(op.density2, 1, mean)
+plot(densities, op.average2, xlab="density",ylab="how many steps to hit gridlock",main="10*15 matrix",
+     pch=".",cex.main=0.8, cex=6, col="blue")
+
+#15*15
+op.density3 <- replicate(100, sapply(densities, function(p) bml.sim(15, 15, p)))
+op.average3 <- apply(op.density3, 1, mean)
+plot(densities, op.average3, xlab="density",ylab="how many steps to hit gridlock",main="15*15 matrix",
+     pch=".",cex.main=0.8, cex=6, col="blue")
+
+#20*20
+op.density4 <- replicate(100, sapply(densities, function(p) bml.sim(20, 20, p)))
+op.average4 <- apply(op.density4, 1, mean)
+plot(densities, op.average3, xlab="density",ylab="how many steps to hit gridlock",main="20*20 matrix",
+     pch=".",cex.main=0.8, cex=6, col="blue")
+
+init.m1<-bml.init(10,10,0.4)
+init.m2<-bml.init(20,20,0.4)
+image(t(apply(init.m1,2,rev)),col=c("white","red","blue"))
+image(t(apply(init.m2,2,rev)),col=c("white","red","blue"))
+
+m <- matrix(sample(c(0,1,2),size=100,replace=T,prob=c((1-0.4),0.4/2,0.4/2)),nrow=10,ncol=10)
+new.m1<-bml.sim1(init.m1)
+image(t(apply(new.m1,2,rev)),col=c("white","red","blue"))
+
+m <- matrix(sample(c(0,1,2),size=400,replace=T,prob=c((1-0.4),0.4/2,0.4/2)),nrow=20,ncol=20)
+new.m2<-bml.sim1(init.m2)
+image(t(apply(new.m2,2,rev)),col=c("white","red","blue"))
+
